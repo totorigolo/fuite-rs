24ae84b6a657286d79a20fdfded9832e9a9742b7
Thomas Lacroix <toto.rigolo@free.fr>
2018-12-02 11:37:50 +0100

    Level name only last 3 seconds.

--- src/systems/text.rs
+++ src/systems/text.rs
@@ -15,6 +15,7 @@ use crate::{
 #[derive(Default)]
 pub struct Text {
     name_display: Option<Entity>,
+    name_display_time_left: Option<f32>,
 
     message_reader: Option<ReaderId<Message>>,
 }
@@ -33,7 +34,7 @@ impl<'s> System<'s> for Text {
         self.message_reader = Some(res.fetch_mut::<MessageChannel>().register_reader());
     }
 
-    fn run(&mut self, (_time, mut ui_text, finder, message_channel, level): Self::SystemData) {
+    fn run(&mut self, (time, mut ui_text, finder, message_channel, level): Self::SystemData) {
         // Getting UI entities
         if let None = self.name_display {
             if let Some(name_ui_entity) = finder.find("name") {
@@ -55,11 +56,26 @@ impl<'s> System<'s> for Text {
                     if let Some(name_display) = ui_text
                         .get_mut(self.name_display.expect("Name UI text is None.")) {
                         name_display.text = name;
+                        self.name_display_time_left = Some(3.0);
                     } else {
                         error!("Failed to change the level name!");
                     }
                 }
                 _ => {}
+            }
+        }
+
+        // Update/manage display time
+        if let Some(mut time_left) = self.name_display_time_left {
+            time_left -= time.delta_seconds();
+            if time_left <= 0.0 {
+                if let Some(name_display) = ui_text
+                    .get_mut(self.name_display.expect("Name UI text is None.")) {
+                    name_display.text = "".into();
+                    self.name_display_time_left = None;
+                }
+            } else {
+                self.name_display_time_left = Some(time_left);
             }
         }
     }
